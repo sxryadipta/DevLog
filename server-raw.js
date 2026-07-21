e1451638d46155566b2e966fe47bc511c862bddf
Suryadipta Ghosh <gsuryadipta@gmail.com>
2026-07-21 20:06:45 +0530

    changing response methods

--- server-raw.js
+++ server-raw.js
@@ -3,7 +3,7 @@ import http from "http";
 import url from "url";
 
 const myServer = http.createServer((req,res) => {
-  res.end("Hi Surya!")
+  res.write("Hi Surya!")
 }).listen(8000);
 console.log("Server has started at port 8000");
 

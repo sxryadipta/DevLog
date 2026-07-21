720201e8b96230337459285cbace877e9859497c
Suryadipta Ghosh <gsuryadipta@gmail.com>
2026-07-21 21:38:47 +0530

    Default response

--- server-raw.js
+++ server-raw.js
@@ -3,8 +3,7 @@ import http from "http";
 import url from "url";
 
 const myServer = http.createServer((req,res) => {
-  res.write("Hi Surya!")
-  res.end()
+  res.end("Hi Surya!")
 }).listen(8000);
 console.log("Server has started at port 8000");
 
